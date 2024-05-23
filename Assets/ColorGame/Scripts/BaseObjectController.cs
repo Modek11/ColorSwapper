1d1659490e417b923849f6f8b29d38b5b70c1866
Modekk <modek11@op.pl>
2024-05-23 17:07:14 +0200

    Base logic for obstacles and collectibles

--- Assets/ColorGame/Scripts/BaseObjectController.cs
+++ Assets/ColorGame/Scripts/BaseObjectController.cs
@@ -2,21 +2,32 @@ using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 
-namespace ColorGame.Scripts.Obstacles
+namespace ColorGame.Scripts
 {
-    public class BaseObstacleHandler : MonoBehaviour
+    public class BaseObjectController : MonoBehaviour
     {
-        [SerializeField] private float rotationDuration;
-        [SerializeField] private List<SpriteRenderer> colorA;
-        [SerializeField] private List<SpriteRenderer> colorB;
-        [SerializeField] private List<SpriteRenderer> colorC;
-        [SerializeField] private List<SpriteRenderer> colorD;
+        [SerializeField] protected bool enableRotation;
+        [SerializeField] protected bool invertRotation;
+        [SerializeField] protected float rotationDuration;
+        [SerializeField] protected List<SpriteRenderer> colorA;
+        [SerializeField] protected List<SpriteRenderer> colorB;
+        [SerializeField] protected List<SpriteRenderer> colorC;
+        [SerializeField] protected List<SpriteRenderer> colorD;
 
         protected void Start()
         {
             SetupColors();
 
+            if (enableRotation)
+            {
+                StartRotating();
+            }
+        }
+
+        protected void StartRotating()
+        {
             var rotation = new Vector3(0, 0, 180);
+            rotation = invertRotation ? rotation * -1 : rotation;
             transform.DORotate(rotation, rotationDuration).SetLoops(-1, LoopType.Incremental).SetEase(Ease.Linear)
                 .SetLink(gameObject, LinkBehaviour.KillOnDisable);
         }
---
+++ Assets/ColorGame/Scripts/Colors/Globals/GameTags.cs
@@ -0,0 +1,9 @@
+
+namespace ColorGame.Scripts.Colors.Globals
+{
+    public static class GameTags
+    {
+        public static string Obstacle = "Obstacle";
+        public static string Collectable = "Collectable";
+    }
+}
